7a29221d441f45f463922032cbb88ae4df06db38
Dennis Iversen <dennis.iversen@gmail.com>
2016-11-22 20:47:34 +0100

    add source

---
+++ gravatar.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace diversen;
+
+/**
+ * Contains simple methods and functions for getting a gravatar image from
+ * gravatar.com 
+ */
+class gravatar {
+
+    public static function getGravatar($email, $s = 80, $d = 'identicon', $r = 'g', $img = false, $atts = array()) {
+        return self::get_gravatar($email, $s, $d, $r, $img, $atts);
+    }
+
+    public static function getGravatarImg($email, $s = 80, $d = 'identicon', $r = 'g', $img = true, $atts = array()) {
+        return self::get_gravatar_img($email, $s, $d, $r, $img, $atts);
+    }
+
+    /**
+     * Get either a Gravatar URL or complete image tag for a specified email address.
+     * @link http://gravatar.com/site/implement/images/php/
+     * @param string $email The email address
+     * @param int $s Size in pixels, defaults to 80px [ 1 - 512 ]
+     * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+     * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+     * @param boolean $img True to return a complete IMG tag False for just the URL
+     * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+     * @return string $str containing either just a URL or a complete image tag
+     */
+    public static function get_gravatar($email, $s = 80, $d = 'identicon', $r = 'g', $img = false, $atts = array()) {
+        $url = 'https://www.gravatar.com/avatar/';
+        $url .= md5(strtolower(trim($email)));
+        $url .= "?s=$s&amp;d=$d&amp;r=$r";
+        if ($img) {
+            $url = '<img alt="Gravatar image" src="' . $url . '"';
+            foreach ($atts as $key => $val) {
+                $url .= ' ' . $key . '="' . $val . '"';
+            }
+            $url .= ' />';
+        }
+        return $url;
+    }
+
+    /**
+     * @ignore
+     */
+    public static function get_gravatar_img($email, $s = 80, $d = 'identicon', $r = 'g', $img = true, $atts = array()) {
+        return self::get_gravatar($email, $s, $d, $r, $img, $atts);
+    }
+}
+
---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+use diversen\gravatar;
+
+// You will not need this is autoloading is set up normally
+// It is just for testing
+
+include_once "../../autoload.php";
+
+$email = 'test@test.com';
+$size = 120;
+
+// Get image tag
+echo gravatar::getGravatarImg($email, $size);
+
+// Full options: 
+// gravatar::getGravatarImg($email, $s = 80, $d = 'identicon', $r = 'g', $img = true, $atts = array())
+
+// Only get tag
+echo gravatar::getGravatar($email, $size);
+
+// Full options: 
+// gravatar::getGravatar($email, $s = 80, $d = 'identicon', $r = 'g', $img = false, $atts = array())
+
